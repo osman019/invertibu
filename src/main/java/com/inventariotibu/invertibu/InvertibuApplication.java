f2a1ca4a40b3a235b82b06dc90f03f2fb6bf6ba0
osman019 <osmanortizrolon@gmail.com>
2025-04-28 16:03:53 -0500

    feat: :sparkles: initialize new proyect

---
+++ src/main/java/com/inventariotibu/invertibu/InvertibuApplication.java
@@ -0,0 +1,13 @@
+package com.inventariotibu.invertibu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvertibuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvertibuApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/inventariotibu/invertibu/InvertibuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inventariotibu.invertibu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvertibuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
